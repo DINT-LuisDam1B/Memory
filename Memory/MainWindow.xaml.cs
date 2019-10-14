c2c678096b2668150304b37b4c9ec353d481d63c
LuisDam1B <luis.narvaez@iesdoctorbalmis.com>
2019-10-14 15:02:34 +0200

    Añadir Dinamicament el GRID

--- Memory/MainWindow.xaml.cs
+++ Memory/MainWindow.xaml.cs
@@ -18,11 +18,42 @@ namespace Memory
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
+        public const int NUMCOLUMNAS = 4;
+
+        public void CrarTabla(int numFilas)
+        {
+
+            for (int i = 0; i < numFilas; i++)
+            {
+                Grid_Imagenes.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int i = 0; i < NUMCOLUMNAS; i++)
+            {
+                for (int j = 0; j < numFilas; j++)
+                {
+                    TextBlock textBlock = new TextBlock();
+                    Grid_Imagenes.Children.Add(textBlock);
+                  
+                    textBlock.
+                }
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_Iniciar(object sender, RoutedEventArgs e)
+        {
+            int numFilas = (int)((RadioButton)sender).Tag;
+            CrarTabla(numFilas);
+
         }
     }
 }
